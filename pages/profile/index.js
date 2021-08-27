89443d9fd15672eaa52827c78f69449f767c18e5
Alex <alexokoro342@gmail.com>
2021-08-27 13:39:53 +0100

    update

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../redux/reducers/userReducer";
 import Meta from "../../src/components/Head/Meta";
