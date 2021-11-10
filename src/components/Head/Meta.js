643fcc689a0b6e4132760cf3a933bb9af3d53ad1
Taiwo Sulaiman <tsulaimon96@gmail.com>
2021-11-10 10:53:53 +0100

    chat

--- src/components/Head/Meta.js
+++ src/components/Head/Meta.js
@@ -57,6 +57,20 @@ const Meta = ({ title, keywords, description }) => {
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
+                    (function(w,d,v3){
+                        w.chaportConfig = {
+                          appId : '61828b3b395e6219c372173e'
+                        };
+                        
+                        if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
+                        
+        `,
+                }}
+            />
+
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
