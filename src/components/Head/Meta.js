65033dfbbd7361732b9aa01fcf57684f43b8217a
Taiwo Sulaiman <tsulaimon96@gmail.com>
2021-11-16 18:53:43 +0100

    check

--- src/components/Head/Meta.js
+++ src/components/Head/Meta.js
@@ -6,6 +6,14 @@ const Meta = ({ title, keywords, description }) => {
     return (
         <Head>
             <link rel="icon" href="/fav.svg" />
+            <meta charset="UTF-8" />
+            <meta name="description" content="Free Web tutorials" />
+            <meta name="keywords" content="HTML, CSS, JavaScript" />
+            <meta name="author" content="John Doe" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
             <script
                 type="module"
                 property="og:image"
