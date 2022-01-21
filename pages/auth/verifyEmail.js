e0fef433de6cdcaaa6773aca2e295087333cf1ac
Alex <alexokoro342@gmail.com>
2022-01-21 20:24:19 +0100

    update

---
+++ pages/auth/verifyEmail.js
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { enviroment } from "../../src/components/enviroment"
+
+function VerifyEmail() {
+    const router = useRouter()
+    const token = router.query()
+
+    useEffect(() => {
+        verifyUser()
+        return () => {
+            verifyUser()
+        }
+    }, [])
+
+    const verifyUser = () => {
+        var requestOptions = {
+            method: 'POST',
+            redirect: 'follow',
+            body: token
+        };
+          
+        fetch(enviroment.BASE_URL + "auth/verify/", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            if(result.error == false) {
+                router.push("/")
+            }
+        })
+        .catch(error => console.log('error', error));
+    }
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VerifyEmail
