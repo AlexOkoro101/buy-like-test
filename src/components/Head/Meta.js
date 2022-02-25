962741730882ed3087d7c809546d8a85cdc62501
Alex <alexokoro342@gmail.com>
2022-02-25 11:45:00 +0100

    update

--- src/components/Head/Meta.js
+++ src/components/Head/Meta.js
@@ -127,4 +127,15 @@ Meta.defaultProps = {
   description: "Buy cars like dealers with ease",
 };
 
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // const res = await fetch(`https://.../data`)
+  const data = null
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
 export default Meta;
+
